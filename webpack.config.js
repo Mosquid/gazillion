f2962a05bb3631e50f7c99ba10df1e5a6461a359
mosquid <mosquid@gmail.com>
2020-10-21 15:12:16 +0300

    website is on the way

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path")
+const fs = require("fs")
+
+module.exports = {
+  entry: "./website/src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./website/dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    port: 9000,
+    contentBase: path.resolve(__dirname, "./website"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, //using regex to tell babel exactly what files to transcompile
+        exclude: /node_modules/, // files to be ignored
+        use: {
+          loader: "babel-loader", // specify the loader
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+}
